f343969198d4d2d003d37ad85700972e35b05b0f
Leon Allen <leon.allen22@gmail.com>
2017-03-29 16:20:27 -0500

    Adding scripts

---
+++ scripts/KDDTools.r
@@ -0,0 +1,86 @@
+#This script is just a quick something I threw together as I was
+#playing with the visualizations we went over in class.
+#Its purpose is mainly to distill the functions introduced by the slides
+#and their basic useage. Not meant to cover all or even most use cases
+#of the covered functions.
+
+#Randomly chooses a sample of size 'n' from a dataframe 'data'
+getSample = function(data, n)
+{
+  data[sample(nrow(data), n),]
+}
+
+#Separate the date colunm from trainTraj into a date and time column
+sepTrajDate = function(data)
+{
+  separate(data, starting_time, c("date", "starting time"), sep=" ")
+}
+
+#Example merge, use to merge 2 matrices together by associating
+#columns of the same name from each matrix
+exMerge = function(data1, data2)
+{
+  #This will associate the "date" columns in data1 and data2
+  #I used the date from trainTraj and trainWeather to merge the
+  #matrices together, then visualize the effect of precipitation
+  #on travel_time
+  merge(data1, data2, by=c("date"))
+}
+
+#Generate density plot of chosen variable
+densPlot = function(data, v)
+{
+  ggplot(data) + geom_density(aes(v))
+}
+
+#Generate histogram of chosen variable
+histPlot = function(data, v)
+{
+  ggplot(data) + geom_histogram(aes(v))
+}
+
+#Generate bar plot of chosen variable
+barPlot = function(data, v)
+{
+  ggplot(data) + geom_bar(aes(v))
+}
+
+#Generate boxplot of chosen variables
+#Note: factor() converts continuous variables into discrete variables
+#This is important b/c variables like vehicle_id are treated as continuous
+#by default, but are really discrete.
+boxPlot = function(data, x, y)
+{
+  ggplot(data, mapping = aes(factor(x), y)) + geom_boxplot()
+}
+
+#Generate a plot of 2 categorical variables
+countPlot = function(data, x, y)
+{
+  ggplot(data) + geom_count(aes(x, y))
+}
+
+#Generate a scatter plot of chosen variables
+scatterPlot = function(data, x, y)
+{
+  ggplot(data, aes(x, y)) + geom_point()
+}
+
+#Generate a scatter plot with linear fit of chosen variables
+linFit = function(data, x, y)
+{
+  ggplot(data, aes(x, y)) + geom_point() + stat_smooth(method="lm")
+}
+
+plotAvgTravelTime <- function(df)
+{
+  qplot(time_window_pos, y = avg_travel_time, data = df, color = Route, geom = "smooth", 
+        xlab = "Date/Time", ylab = "Avg. Travel Time (seconds)",
+        main = "Average Travel Time vs Date based on Route")
+}
+
+plotAvgVolume <- function(df)
+{
+  qplot(time_window_pos, y = volume, data = df, color = Tollgate_And_Direction, geom = "smooth", 
+        xlab = "Date/Time", ylab = "Avg. Volume", main = "Average Volume vs Date based on Tollgate Entry/Exit")
+}
---
+++ scripts/setup.r
@@ -0,0 +1,44 @@
+
+#Load any libraries used in the scripts I wrote
+setup = function()
+{
+  library("ggplot2")
+  library("stringr")
+  library("dplyr")
+  require("tidyr")
+}
+
+#Load the data files provided by the KDD hosts into dataframes
+#pathToData: path to the directory containing the "testing_phase1" and "training" directories
+readData = function(pathToData)
+{
+  trainLinks <<- read.csv(paste(pathToData,"training/links (table 3).csv", sep=""))
+  trainRoutes <<- read.csv(paste(pathToData,"training/routes (table 4).csv", sep=""))
+  trainTraj <<- read.csv(paste(pathToData,"training/trajectories(table 5)_training.csv", sep=""))
+  trainVolume <<- read.csv(paste(pathToData,"training/volume(table 6)_training.csv", sep=""))
+  trainWeather <<- read.csv(paste(pathToData,"training/weather (table 7)_training.csv", sep=""))
+  train20Travel <<- read.csv(paste(pathToData, "training/training_20min_avg_travel_time.csv", sep=""))
+  train20Volume <<- read.csv(paste(pathToData, "training/training_20min_avg_volume.csv", sep=""))
+  testTraj <<- read.csv(paste(pathToData,"testing_phase1/trajectories(table 5)_test1.csv", sep=""))
+  testVolume <<- read.csv(paste(pathToData,"testing_phase1/volume(table 6)_test1.csv", sep=""))
+  testWeather <<- read.csv(paste(pathToData,"testing_phase1/weather (table 7)_test1.csv", sep=""))
+  test20Travel <<- read.csv(paste(pathToData, "testing_phase1/test1_20min_avg_travel_time.csv", sep=""))
+  test20Volume <<- read.csv(paste(pathToData, "testing_phase1/test1_20min_avg_volume.csv", sep=""))
+}
+
+setupTravelTime20Avg <- function(df)
+{
+  df$time_window_pos <- strptime(str_sub(df$time_window, start = 2, end = 20), format = "%Y-%m-%d %H:%M:%S")
+  df$time_window_num <- as.numeric(df$time_window_pos)
+  df$Route <- as.factor(paste("I", as.character(df$intersection_id), " -> T", as.character(df$tollgate_id), sep = ""))
+  return(df)
+}
+
+setupVolume20Avg <- function(df)
+{
+  df$time_window_pos <- strptime(str_sub(df$time_window, start = 2, end = 20), format = "%Y-%m-%d %H:%M:%S")
+  df$time_window_num <- as.numeric(df$time_window_pos)
+  df$exiting <- ifelse(df$direction == 1, "exit", "entry")
+  df$Tollgate_And_Direction <- as.factor(paste("T", as.character(df$tollgate_id), " - ", df$exiting, sep = ""))
+  return(df)
+}
